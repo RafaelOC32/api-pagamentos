da24ca0a2df0f010cdcbca274088fbf5d5523d7c
Rafael de Oliveira Carvalhal <rafaeldeoliveiracarvalhal@outlook.com>
2026-08-05 17:11:54 -0300

    v1.1.0

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("api-pagamentos iniciada");
-console.log("Projeto final integrador DevSecOps");
+console.log("Projeto final integrador DevSecOps ADA");
 console.log("Compliance continuo: Policy as Code, SBOM, assinatura e attestations");
